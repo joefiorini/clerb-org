64743b10b247b313651365ee3430c7a860c70192
Joe Fiorini <joe@joe-fiorinis-macbook-pro.local>
2008-08-16 15:59:30 -0400

    Cleaned up login and overview admin pages; modified permissions for apache, not sure how to put that in separate commit :(

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -69,6 +69,8 @@ class PostsController < ResourceController::Base
   def load_collection
 		if params[:tag]
 			@posts = Post.find_tagged_with params[:tag]
+		elsif @current_user
+		  @posts = Post.posts_per_date
 		else
       @posts = Post.not_sticky.all  :limit => 10, :order => "created_at desc"
 		end
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,12 @@ class Post < ActiveRecord::Base
 	acts_as_taggable
 	named_scope :sticky, :conditions => ["sticky = 't' and ((type = 'Event' and start_time > ?) or type is null)", DateTime.now], :order => 'start_time desc'
   named_scope :not_sticky, :conditions => {:sticky => false}
+  named_scope :published#not respecting draft status yet, :conditions => {:is_published => true, :is_published => false, :is_published => nil}
+  
+  def self.posts_per_date
+    published.to_set.classify { |post| post.created_at.to_date }
+  end
+  
 	def generate_permalink
 		#raise self.attributes.to_s
 		if self.permalink.nil?
