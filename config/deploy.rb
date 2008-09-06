6ba4bb23cc306839644797060ccf37c831a94113
Joe Fiorini <joe@joe-fiorinis-macbook-pro.local>
2008-09-06 00:13:08 -0400

    Updated deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,7 @@
 set :application, "clerb.org"
 set :repository,  "git://git.densitypop.net/clerb.git"
 set :scm, :git
+set :git_enable_submodules, 1
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
