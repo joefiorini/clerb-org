199da42083af84c21bdc3e441b0581c80ca02703
faithfulgeek <joe@faithfulgeek.org>
2009-01-31 19:15:14 -0500

    Rails 2.2.2

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ require 'rubygems'
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.2.0' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.2.2' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -60,6 +60,7 @@ Rails::Initializer.run do |config|
   # config.active_record.default_timezone = :utc
   
   config.gem 'haml'
+  config.gem 'ruby-openid'
 end
 
 require "#{File.expand_path(RAILS_ROOT)}/lib/string"
