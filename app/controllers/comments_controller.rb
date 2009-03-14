8a2e36df90e2a74be0d68f6cd69e9a1e7a0e62f4
faithfulgeek <joe@faithfulgeek.org>
2009-03-14 01:19:21 -0400

    Removed the (hopefully) last vestige of comments code

--- app/controllers/comments_controller.rb
+++
@@ -1,7 +0,0 @@
-class CommentsController < ResourceController::Base
-  belongs_to :post
-	has_rakismet
-	
-	create.wants.html { redirect_to @post }
-	create.failure.wants.html { redirect_to @post }
-end
